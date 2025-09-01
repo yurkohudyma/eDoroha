ebd843a769bacba217be0de647fa562574b5c850
yurkohudyma <hudyma@gmail.com>
2025-09-01 10:25:39 +0300

    init commit

---
+++ src/main/java/ua/hudyma/EDorohaApplication.java
@@ -0,0 +1,11 @@
+package ua.hudyma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDorohaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EDorohaApplication.class, args);
+    }
+}
